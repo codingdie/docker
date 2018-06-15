34db5fc57e184df11152fa4e6879ec9520b5f3cd
codingdie <547912355@qq.com>
2018-06-15 20:48:59 +0800

    fix

--- kafka/install.sh
+++ kafka/install.sh
@@ -2,6 +2,6 @@ docker build -t kafka:1.0  .
 name=kafka
 docker stop ${name}
 docker rm ${name}
-docker run --name ${name}  -p 9092:9092 -d  -e host=$1  kafka:1.0
+docker run --name ${name}  -p 9092:9092 -p 2181:2181 -d  -e host=$1  kafka:1.0
 
 
